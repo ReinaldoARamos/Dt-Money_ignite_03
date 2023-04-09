8249fca7d7a6264f975b16f2e0f3837a93c446e6
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-04-09 18:44:36 -0300

    Aplicando a biblioteca e também criando um outro componente que ira comporatar o modal

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,25 +1,19 @@
 import { HeaderContainer, HeaderContent, TransactionButton } from "./styles";
 import HeaderLogo from "../../assets/Logo.svg";
 import * as Dialog from "@radix-ui/react-dialog";
+import { NewTransactionModal } from "../NewTransaction";
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={HeaderLogo} />
-        <Dialog.Root>
+        <Dialog.Root> 
             <Dialog.Trigger asChild>
           <TransactionButton>Nova Transação</TransactionButton>
           </Dialog.Trigger>
-          <Dialog.Portal>
-             <Dialog.Overlay />
-                <Dialog.Content>
-                    <Dialog.Title>
-                    Nova Transação
-                    </Dialog.Title>
-                    <Dialog.Close />
-                </Dialog.Content>
-            
-          </Dialog.Portal>
+            <NewTransactionModal />
+
+          
         </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
