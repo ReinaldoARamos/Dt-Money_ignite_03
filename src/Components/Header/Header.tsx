944e49e05ce3952ef58e2b6da05aac92d0780c66
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-04-09 17:50:58 -0300

    criando os Dialogs que renderizam modais(abre e fecha)

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,17 +1,27 @@
 import { HeaderContainer, HeaderContent, TransactionButton } from "./styles";
-import HeaderLogo from "../../assets/Logo.svg"
+import HeaderLogo from "../../assets/Logo.svg";
+import * as Dialog from "@radix-ui/react-dialog";
 export function Header() {
-   
-    return(
-        
-        <HeaderContainer>
-
-        <HeaderContent>
-        <img src={HeaderLogo}/>    
-        <TransactionButton>Nova Transação</TransactionButton>
-        </HeaderContent>
-
-        </HeaderContainer>
-    
-    )
+  return (
+    <HeaderContainer>
+      <HeaderContent>
+        <img src={HeaderLogo} />
+        <Dialog.Root>
+            <Dialog.Trigger asChild>
+          <TransactionButton>Nova Transação</TransactionButton>
+          </Dialog.Trigger>
+          <Dialog.Portal>
+             <Dialog.Overlay />
+                <Dialog.Content>
+                    <Dialog.Title>
+                    Nova Transação
+                    </Dialog.Title>
+                    <Dialog.Close />
+                </Dialog.Content>
+            
+          </Dialog.Portal>
+        </Dialog.Root>
+      </HeaderContent>
+    </HeaderContainer>
+  );
 }
