e4f24c31b885d691fbcd419c98db2fb9f6c21512
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-04-11 17:17:20 -0300

    criando os schemas do modal

--- src/Components/NewTransaction/index.tsx
+++ src/Components/NewTransaction/index.tsx
@@ -1,9 +1,26 @@
 import * as Dialog from "@radix-ui/react-dialog";
 import { Overlay, Content, CloseButton, TransactionType, TransactionTypeButton } from "./style";
 import { ArrowCircleDown, ArrowCircleUp, X } from "phosphor-react";
+import * as z from 'zod'
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
+const newTransactionFormSchema = z.object(
+  {
+    description: z.string(),
+    price: z.number(),
+    category: z.string(),
+    type: z.enum(['income', 'outcome'])
+  }
+)
+
+type newTransactionFormInputs = z.infer<typeof newTransactionFormSchema>
 
 export function NewTransactionModal() {
+
+  const {register, handleSubmit} = useForm<newTransactionFormInputs>({
+    resolver: zodResolver(newTransactionFormSchema)
+  });
   return (
     <Dialog.Portal>
      <Overlay/>
@@ -13,9 +30,9 @@ export function NewTransactionModal() {
             <X size={22}/>
         </CloseButton>
         <form action=""> 
-            <input type="text" placeholder="descrição"/>
-            <input type="number" placeholder="preço"/>
-            <input type="text" placeholder="catergoria"/>
+            <input type="text" placeholder="descrição" {...register('description') }/>
+            <input type="number" placeholder="preço" {...register('price', {valueAsNumber: true}) }/>
+            <input type="text" placeholder="catergoria" {...register('category') }/>
 
 
             <TransactionType>
