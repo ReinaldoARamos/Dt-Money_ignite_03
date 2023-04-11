244847262552470aa5295837e2a9e8b76fa79f34
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-04-11 15:24:31 -0300

    Criando o schema do zod

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,15 +1,26 @@
 import { useForm } from "react-hook-form";
 import { SearchButton, SearchContainer, SearchFormInput } from "./style";
 import { MagnifyingGlass } from "phosphor-react";
-export function SearchForm() {
+import * as z from 'zod'
 
-  const {register, handleSubmit} = useForm();
+const searchFormSchema = z.object({
+  query: z.string(),
+})
+
+type SearchFormInputs = z.infer<typeof searchFormSchema>
+
+export function SearchForm() {
+  
+
+  const { register, handleSubmit } = useForm();
+  
+  
   function handleSearchTransactions() {
-    
+
   }
   return (
-    <SearchContainer onSubmit={handleSubmit}>
-      <SearchFormInput placeholder="Busque uma transação"  {...register('query')}/>
+    <SearchContainer onSubmit={handleSubmit(handleSearchTransactions)}>
+      <SearchFormInput placeholder="Busque uma transação"  {...register('query')} />
 
       <SearchButton>
         <MagnifyingGlass size={22} color="#00B37E" />
