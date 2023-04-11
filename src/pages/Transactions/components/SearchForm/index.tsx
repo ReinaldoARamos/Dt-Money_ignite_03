640b247b19633d48b0742725925e869f960607cf
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-04-11 15:20:34 -0300

    instalando o zod e aplicando o register

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,9 +1,15 @@
+import { useForm } from "react-hook-form";
 import { SearchButton, SearchContainer, SearchFormInput } from "./style";
 import { MagnifyingGlass } from "phosphor-react";
 export function SearchForm() {
+
+  const {register, handleSubmit} = useForm();
+  function handleSearchTransactions() {
+    
+  }
   return (
-    <SearchContainer>
-      <SearchFormInput placeholder="Busque uma transação" />
+    <SearchContainer onSubmit={handleSubmit}>
+      <SearchFormInput placeholder="Busque uma transação"  {...register('query')}/>
 
       <SearchButton>
         <MagnifyingGlass size={22} color="#00B37E" />
